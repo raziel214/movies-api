9829f77d7c4afb3fe0ed869b09d7ab25205e6103
raziel214 <raziel214@users.noreply.github.com>
2021-01-29 22:44:27 -0500

    Setting movies

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
+const {
+  logErrors,
+  errorHandler
+} = require('./utils/middleware/errorHandlers.js');
+
+// body parser
+app.use(express.json());
+
 moviesApi(app);
 
-app.listen(config.port, function () {
-    console.log(`Listening http://localhots:${config.port}`);
+app.use(logErrors);
+app.use(errorHandler);
+
+app.listen(config.port, function() {
+  console.log(`Listening http://localhost:${config.port}`);
 });
